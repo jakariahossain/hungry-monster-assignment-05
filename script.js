7f2615d124a188b312afe4aea20d3089c59bb5a7
jakariahossain <rootcomsp@gmail.com>
2021-02-07 19:19:17 +0600

    Fixed Food Items Search Issues

--- script.js
+++ script.js
@@ -1,86 +1,62 @@
-// Display Meals From API
-// Search And Show Search Result
-// Click Each Meal Item And Show Details
-
 const mealItems = document.getElementById("meal-list");
-const searchInput = document.getElementById('search-input');
-const searchBtn = document.getElementById('search-btn');
-const modal = document.getElementById('modal');
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const modal = document.getElementById("modal");
 const closeModalBtn = document.getElementById("close-modal");
 
+// Search Meal Items From API
+searchBtn.addEventListener("click", () => {
+  const inputValue = searchInput.value;
+  getMealItems(inputValue);
+  searchInput.value = "";
+});
 
 // Display Meal Items From API
 
-getMealItems();
-
-function getMealItems() {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
-  .then(res => res.json())
-  .then(data => {
-   
-    displayMealItems(data);
-  }).catch(err => console.log(err));
-
-  
+function getMealItems(inputValue) {
+  const fetchItem = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+  fetch(fetchItem)
+    .then((res) => res.json())
+    .then((data) => {
+      displayMealItems(data);
+    })
+    .catch((err) => console.log(err));
 }
 
 function displayMealItems(items) {
   mealItems.innerHTML = "";
-
-    const getItem = items.meals;
-    getItem.forEach(item =>{
-
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('card');
-    itemDiv.innerHTML = `
+  const getItem = items.meals;
+  if (getItem != null) {
+    getItem.forEach((item) => {
+      const itemDiv = document.createElement("div");
+      itemDiv.classList.add("card");
+      itemDiv.innerHTML = `
           <div class="card-body">
             <img src="${item.strMealThumb}" alt="Germany" />
             <h2 class="item-name">${item.strMeal}</h2>
           </div>
     
     `;
+      // console.log(item);
+      itemDiv.addEventListener("click", () => {
+        modal.style.display = "flex";
+        displayMealIDetails(item);
+      });
 
-    // console.log(item);
-
-    itemDiv.addEventListener('click', () => {
-      modal.style.display = 'flex';
-      displayMealIDetails(item);
-    })
-
-    mealItems.appendChild(itemDiv);
+      mealItems.appendChild(itemDiv);
     });
-
-    
+  } else {
+    mealItems.innerHTML = `
+      
+      <h4>Sorry ! The Item you are looking for is not Available. <br> Please search another item </h4>
+    `;
+  }
 }
 
-// Search Meal Items From API
-searchBtn.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    const itemName = document.querySelectorAll('.item-name');
-    
-    itemName.forEach(foodItem => {
-      // Hide all Parent Items Except Search Value
-      // .card --> .item-name --> .card-body 
-        if(foodItem.innerText.toLowerCase().includes(inputValue.toLowerCase())){
-          foodItem.parentElement.parentElement.style.display = "block";
-        }else{
-          foodItem.parentElement.parentElement.style.display = "none";
-          
-        }
-
-        
-    });
-
-    // console.log(inputValue);
-
-    searchInput.value = '';
-});
-
-// Modal 
-function displayMealIDetails(item){
-  
-    const modalBody = document.querySelector('.modal-body');
-    modalBody.innerHTML = `
+// Modal
+function displayMealIDetails(item) {
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = `
 
       <img src="${item.strMealThumb}" >
       <div class="modal-content">
@@ -95,12 +71,10 @@ function displayMealIDetails(item){
             <li><i class="fas fa-check-square"></i> ${item.strIngredient6}</li>   
         </ul>
       </div>
-    `;  
+    `;
 }
-
 
 // Close the modal
 closeModalBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
